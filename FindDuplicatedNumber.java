79862d538ed8164fec986684f35660a4fb30f4b3
lovymy <15002924939@163.com>
2017-04-19 18:19:01 +0800

    Create FindDuplicatedNumber.java
    It's beyond my intelligence...

---
+++ FindDuplicatedNumber.java
@@ -0,0 +1,34 @@
+public class FindDuplicatedNumber{
+  public int findDuplicate(int[] nums) {
+    int n = nums.length;
+    for(int i=0;i<nums.length;i++) nums[i]--;
+    int slow = n-1;
+    int fast = n-1;
+    do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }while(slow != fast);
+    slow = n-1;
+    while(slow != fast){
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow+1;
+  }
+  
+  public int findDuplicate2(int[] nums) {
+    int n = nums.length;
+    int slow = n;
+    int fast = n;
+    do{
+        slow = nums[slow-1];
+        fast = nums[nums[fast-1]-1];
+    }while(slow != fast);
+    slow = n;
+    while(slow != fast){
+        slow = nums[slow-1];
+        fast = nums[fast-1];
+    }
+    return slow;
+  }
+}
