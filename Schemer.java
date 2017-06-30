639be1637702850523a9d1835d7e3cae5230db72
lovymy <15002924939@163.com>
2017-06-30 17:39:15 +0800

    Create Schemer.java
    There are n different online courses numbered from 1 to n. Each course 
    Given n online courses represented by pairs (time,end), your task is to find the maximal number of courses that can be taken.

---
+++ Schemer.java
@@ -0,0 +1,20 @@
+public class Schemer{
+  public int scheduleCourse(int[][] courses) {
+        if(courses.length==0)
+            return 0;
+        Arrays.sort(courses, Comparator.comparingInt(a -> a[1]));
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+
+        int start=0;
+        for(int[] course:courses){
+            start+=course[0];
+            pq.offer(course[0]);
+            if(start>course[1]){
+                pq.poll();
+                start-=course[1];
+            }
+        }
+        return pq.size();
+    }
+  }
+}
